7e89130489242dd12ad4ed9152dafbefed9341c9
Sergey Ponomarev <stokito@gmail.com>
2013-10-30 04:16:30 +0200

    Forked issues

--- CurrenciesGrailsPlugin.groovy
+++ CurrenciesGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class CurrenciesGrailsPlugin {
     String grailsVersion = '2.0.0 > *'
     String author = 'Ricardo J. Méndez'
     String authorEmail = 'ricardo@arquetipos.co.cr'
-    String title = 'Currency and exchange rate plugin for grails'
+    String title = 'Currency and exchange rate plugin for Grails'
     String description = '''\
 Allows easy storage of currencies and exchange rates across them.  Provides
 a Money helper class that is used for converting between currencies at the
@@ -12,4 +12,8 @@ exchange rate on a certain date (the current date is used by default).
 
 It's recommended you use the money amounts embedded.
 '''
+    String documentation = 'https://github.com/stokito/grails-currencies'
+    String license = 'APACHE'
+    Map issueManagement = [system: 'JIRA', url: 'https://github.com/stokito/grails-currencies/issues']
+    List scm = [url: 'https://github.com/stokito/grails-currencies']
 }
