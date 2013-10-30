69f14469ebd0b6a1af4b9179e9997d4efb9e73d5
Sergey Ponomarev <stokito@gmail.com>
2013-10-30 05:40:18 +0200

    date -> dateCreated to avoid SQL keyword

--- test/integration/ExchangeRateTests.groovy
+++ test/integration/ExchangeRateTests.groovy
@@ -8,10 +8,10 @@ class ExchangeRateTests extends GroovyTestCase {
         def euro = Currency.getInstance('EUR')
         def gbp = Currency.getInstance('GBP')
 
-        new ExchangeRate(baseCurrency:euro, toCurrency:'USD', rate:1.46122, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:yen, toCurrency:dollar, rate:0.008981, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:'USD', toCurrency:yen, rate:111.336, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:gbp, toCurrency:dollar, rate:2.02369, date:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:euro, toCurrency:'USD', rate:1.46122, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:yen, toCurrency:dollar, rate:0.008981, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:'USD', toCurrency:yen, rate:111.336, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:gbp, toCurrency:dollar, rate:2.02369, dateCreated:new Date('2007/12/02')).save()
     }
     
     
@@ -24,6 +24,6 @@ class ExchangeRateTests extends GroovyTestCase {
         def rate = ExchangeRate.findByBaseCurrencyAndToCurrency(euro, dollar)
         assert rate
         assertEquals rate.rate, 1.46122
-        assertEquals rate.date, new Date('2007/12/02')
+        assertEquals rate.dateCreated, new Date('2007/12/02')
     }
 }
--- test/integration/MoneyTests.groovy
+++ test/integration/MoneyTests.groovy
@@ -8,11 +8,11 @@ class MoneyTests extends GroovyTestCase {
         def euro = Currency.getInstance('EUR')
         def gbp = Currency.getInstance('GBP')
 
-        new ExchangeRate(baseCurrency:euro, toCurrency:dollar, rate:1.46122, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:yen, toCurrency:dollar, rate:0.008981, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:dollar, toCurrency:yen, rate:111.336, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:gbp, toCurrency:dollar, rate:2.02369, date:new Date('2007/12/02')).save()
-        new ExchangeRate(baseCurrency:euro, toCurrency:dollar, rate:1.33159, date:new Date('2006/12/02')).save()
+        new ExchangeRate(baseCurrency:euro, toCurrency:dollar, rate:1.46122, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:yen, toCurrency:dollar, rate:0.008981, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:dollar, toCurrency:yen, rate:111.336, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:gbp, toCurrency:dollar, rate:2.02369, dateCreated:new Date('2007/12/02')).save()
+        new ExchangeRate(baseCurrency:euro, toCurrency:dollar, rate:1.33159, dateCreated:new Date('2006/12/02')).save()
     }
 
     void testInits() {
